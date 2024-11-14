f60453d6d4f509334335c12a1609bcf30b543d4f
Liu3420175 <1196201660@qq.com>
2024-11-14 13:51:43 +0800

    fix bug

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -40,7 +40,7 @@ def calculate_string_md5(text: Union[str, bytes]):
     计算字符串的md5值
     """
     if isinstance(text, str):
-        text = text.encode('utf-8')
+        text = text.encode('utf-8', errors='ignore')
     md5_hash = hashlib.md5()
     md5_hash.update(text)
     return md5_hash.hexdigest()
