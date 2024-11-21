63e826daf20fb32ae8e6d5099f67e979729b6df1
Liu3420175 <1196201660@qq.com>
2024-11-21 18:40:42 +0800

    change

--- src/diversefile/abc/__init__.py
+++ src/diversefile/abc/__init__.py
@@ -2,6 +2,9 @@
 
 from abc import ABC, abstractmethod
 
+import readability
+import html_text
+
 TEXT_CHUNK_SIZE = 256  # 默认分词块大小
 TEXT_DELIMITER = {'\n','!','?',';','。','；','！','？'}  # 默认一句话的终止符
 
@@ -19,3 +22,32 @@ class AbstractLoader(ABC):
     @abstractmethod
     def load(self):
         pass
+
+
+class HtmlCommonLoader(AbstractLoader):
+
+    def load(self):
+        if self.is_filepath:
+            with open(self.file_path, 'r', encoding='utf-8', errors='ignore') as f:
+                txt = f.read()
+        else:
+            txt = self.file_path
+        sections = self.parser_txt(txt)
+        current_txt = ''
+        for line in sections:
+            if len(current_txt) >= self.chunk_size:
+                yield current_txt
+                current_txt = ''
+            current_txt +=  line.strip()
+
+
+    @classmethod
+    def parser_txt(cls, txt):
+        if type(txt) != str:
+            raise TypeError("txt type should be str!")
+        html_doc = readability.Document(txt)
+        title = html_doc.title()
+        content = html_text.extract_text(html_doc.summary(html_partial=True))
+        txt = f"{title}\n{content}"
+        sections = txt.split("\n")
+        return sections
--- src/diversefile/markdown_loader.py
+++ src/diversefile/markdown_loader.py
@@ -2,9 +2,8 @@
 import re
 
 from markdown import markdown
-from triton.compiler.compiler import ptx_prototype_pattern
 
-from src.diversefile.abc import AbstractLoader
+from src.diversefile.abc import AbstractLoader, HtmlCommonLoader
 
 class MarkdownLoader(AbstractLoader):
 
@@ -15,44 +14,7 @@ class MarkdownLoader(AbstractLoader):
         else:
             txt = self.file_path
 
-        remainder, tables = self.extract_tables_and_remainder(f'{txt}\n')
-        sections = []
-        tbls = []
-        for sec in remainder.split("\n"):
-            if len(sec) > 10 * self.chunk_size:
-                sections.append((sec[:int(len(sec) / 2)], ""))
-                sections.append((sec[int(len(sec) / 2):], ""))
-            else:
-                if sections and sections[-1][0].strip().find("#") == 0:
-                    sec_, _ = sections.pop(-1)
-                    sections.append((sec_+"\n"+sec, ""))
-                else:
-                    sections.append((sec, ""))
-
-        for table in tables:
-            tbls.append(((None, markdown(table, extensions=['markdown.extensions.tables'])), ""))
-        return sections, tbls
-
-    @staticmethod
-    def extract_tables_and_remainder(markdown_text):
-        table_pattern = re.compile(
-            r'''
-            (?:\n|^)                     
-            (?:\|.*?\|.*?\|.*?\n)        
-            (?:\|(?:\s*[:-]+[-| :]*\s*)\|.*?\n) 
-            (?:\|.*?\|.*?\|.*?\n)+
-            ''', re.VERBOSE)
-        tables = table_pattern.findall(markdown_text)
-        remainder = table_pattern.sub('', markdown_text)
-
-        no_border_table_pattern = re.compile(
-            r'''
-            (?:\n|^)                 
-            (?:\S.*?\|.*?\n)
-            (?:(?:\s*[:-]+[-| :]*\s*).*?\n)
-            (?:\S.*?\|.*?\n)+
-            ''', re.VERBOSE)
-        no_border_tables = no_border_table_pattern.findall(remainder)
-        tables.extend(no_border_tables)
-        remainder = no_border_table_pattern.sub('', remainder)
-        return remainder, tables
+        html_str = markdown(txt)
+        lines = HtmlCommonLoader(html_str, is_filepath=False).load()
+        for line in lines:
+            yield line
